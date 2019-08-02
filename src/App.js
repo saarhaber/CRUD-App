3ce7af1daf15b17cace44b335b9a34adf11d87df
thomas-westfall <thomas.westfall64@hunter.cuny.edu>
2019-08-02 12:09:35 -0400

    add campuses

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 import { connect } from "react-redux";
 import AppView from "./AppView"
 import {fetchStudentsThunk, removeStudentThunk, addStudentThunk } from "./store/utilities/students";
-
+import {fetchCampusesThunk, removeCampusThunk, addCampusThunk } from "./store/utilities/campuses";
 
 class AppContainer extends Component {
   constructor() {
@@ -15,6 +15,7 @@ class AppContainer extends Component {
 
   componentDidMount() {
     this.props.fetchAllStudents();
+    this.props.fetchAllCampuses();
   }
 
   removeStudent = (id) => {
@@ -25,9 +26,18 @@ class AppContainer extends Component {
     this.props.addStudent(student);
   }
 
+  removeCampus = (id) => {
+    this.props.removeCampus(id);
+  }
+
+  addCampus = (campus) => {
+    this.props.addCampus(campus)
+  }
+
   render() {
     return (
-      <AppView students={this.props.students} removeStudent={this.removeStudent} addStudent={this.addStudent} />
+      <AppView students={this.props.students} removeStudent={this.removeStudent} addStudent={this.addStudent} campuses=
+      {this.props.campuses} removeCampus={this.removeCampus} addCampus={this.addCampus} />
     )
   }
 }
@@ -35,7 +45,8 @@ class AppContainer extends Component {
 const mapState = (state) => {
   return {
     counter: state.counter,
-    students: state.students
+    students: state.students,
+    campuses: state.campuses
   }
 }
 
@@ -43,7 +54,10 @@ const mapDispatch = (dispatch) => {
   return {
     fetchAllStudents: () => dispatch(fetchStudentsThunk()),
     removeStudent: (id) => dispatch(removeStudentThunk(id)),
-    addStudent: (student) => dispatch(addStudentThunk(student))
+    addStudent: (student) => dispatch(addStudentThunk(student)),
+    fetchAllCampuses: () => dispatch(fetchCampusesThunk()),
+    removeCampus: (id) => dispatch(removeCampusThunk(id)),
+    addCampus: (campus) => dispatch(addCampusThunk(campus))
   }
 }
 export default connect(mapState, mapDispatch)(AppContainer);
--- src/AppView.js
+++ src/AppView.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 const AppView = (props) => {
-    const { students, removeStudent, addStudent } = props;
+    const { students, removeStudent, addStudent, campuses, removeCampus, addCampus} = props;
 
     const studentToAdd = {
         "id": 2,
@@ -15,10 +15,18 @@ const AppView = (props) => {
         "campusId": 1
     }
 
+
+    const campusToAdd = {
+        "id": 1,
+        "name": "BARUCH COLLEGE",
+        "imageUrl": "https://specials-images.forbesimg.com/imageserve/55ae7abce4b05c2c343212c1/300x300.jpg?fit=scale&background=000000",
+        }
+
     return (
     <div className="App">
         <header className="App-header">
             {students.map(student => <div><img src={student.imageUrl} width="100" height="100" onClick={() => addStudent(studentToAdd)}></img></div>)}
+            {campuses.map(campus => <div><img src={campus.imageUrl} width="100" height="100" onClick={() => addCampus(campusToAdd)}></img></div>)}
         </header>
     </div>
     )
