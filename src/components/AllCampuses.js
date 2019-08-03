6743f696b28b35f425c83a1bd71e7758da3846da
NapatKarm <napat.karm@gmail.com>
2019-08-02 20:44:35 -0400

    Beginning

--- src/components/AllCampuses.js
+++ src/components/AllCampuses.js
@@ -5,20 +5,25 @@ import SingleCampus from './SingleCampus';
 
 const AllCampuses = (props) => {
   const { campuses, removeCampus, addCampus} = props;
-
-  const campusToAdd = {
-    "id": 1,
-    "name": "BARUCH COLLEGE",
-    "imageUrl": "https://specials-images.forbesimg.com/imageserve/55ae7abce4b05c2c343212c1/300x300.jpg?fit=scale&background=000000",
-    }
-
     return (
       <div className="Content">
           <h1> This is all Campuses page and it works</h1>
           <p><Link to="/">Home</Link></p>
           <p><Link to="/addcampus">Add Campus</Link></p>
           
-          {campuses.map(campus => <div><img src={campus.imageUrl} width="100" height="100" onClick={() => addCampus(campusToAdd)}></img></div>)}
+          {campuses.map(campus => 
+          <div>
+            <table className = "campusTable">
+              <tr>
+                <th>{campus.name}</th><img src={campus.imageUrl} width="100" height="100"></img>
+              </tr>
+                <button >Remove</button>
+              <tr>
+                <td></td>
+              </tr>
+            </table>
+          </div>
+          )}
       </div>
     )
   }
