8d8950723986f3f47dd3b4fa40dca85f8b54a1b5
NapatKarm <napat.karm@gmail.com>
2019-08-12 22:32:05 -0400

    Student refresh apparently works

--- src/components/SingleStudent.js
+++ src/components/SingleStudent.js
@@ -1,14 +1,33 @@
 import React, { Component } from 'react';
 import {Link} from 'react-router-dom';
 import './SingleStudent.css';
+import Axios from 'axios';
     
 class SingleStudent extends Component {
     constructor(props) {
         super(props)
         this.state = {
-            student : this.props.student
+            student : {
+                firstName:"",
+                lastName:"",
+                campus : {
+                    name: ""
+                }
+            }
         }
       }
+    async componentDidMount(){
+        try{
+            let {data} = await Axios.get(`https://crud-ntsj.herokuapp.com/api/students/${this.props.match.params.id}`)
+            this.setState({
+                student:data
+            })
+            console.log(data)
+        }
+        catch(err){
+            console.log(err);
+        }
+    }
     render(){
         return (
             <div>
@@ -24,6 +43,12 @@ class SingleStudent extends Component {
                             <th className = "heading"> {this.state.student.firstName} {this.state.student.lastName}</th> <Link className="editButton" to={`/student/edit/${this.state.student.id}`}>Edit</Link>  
                         </tr>
                         <tr>
+                            <td>
+                                <p className="cname">ID: {this.state.student.id}</p>
+                            </td>
+                            <td>
+                                <p className="cname">Attends: {this.state.student.campus.name}</p>
+                            </td>
                         </tr>
                         </tbody>
                     </table>
